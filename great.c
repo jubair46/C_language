f6b086f6a91269393f7c7241b087a5f6cdd80198
jubair46 <jubairahmadshaik@gmail.com>
2026-03-13 10:28:49 +0530

    Newfileadded

---
+++ great.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter three numbers =");
+    scanf("%d,%d,%d"&a,&b,&c);
+
+    if(a==b && a>c) || (a==c && a>b)
+    {
+        printf("same inputs given");
+    }
+    else if(a>b && a>c)
+    {
+        printf("%d",a);
+    }
+    else if(b>a && b>c)
+    {
+        printf("%d",b);
+    }
+    else if(c>a && c>b)
+    {
+        printf("%d",c);
+    }
+    else
+    {
+        printf("all inputss are same")
+    }
+    return 0;
+}
